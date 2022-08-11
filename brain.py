f3449b67f70384ccdcd9411db7b5c18312983200
Batool Zaidi <batool@batools-air.lan>
2022-08-11 12:04:47 -0400

    initial insurance function

--- brain.py
+++ brain.py
@@ -1,4 +1,3 @@
-from operator import truediv
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,13 +5,14 @@ hospitals = [
     ["A.O. Fox Memorial Hospital", "1 Norton Ave, Oneonta, NY 13820", "https://www.bassett.org/locations/ao-fox-hospital/ao-fox-patients-visitors/ao-fox-hospital-pricing"]
 ]
 
-def interpretNLE():
+IPs = [
+    ["A.O. Fox Memorial Hospital", "1 Norton Ave, Oneonta, NY 13820", "https://www.bassett.org/locations/ao-fox-hospital/ao-fox-patients-visitors/ao-fox-hospital-pricing"]
+]
+
+def interpretNLE(nl):
     pass
 
 def is_downloadable(url):
-    """
-    Does the url contain a downloadable resource
-    """
     h = requests.head(url, allow_redirects=True)
     header = h.headers
     content_type = header.get('content-type')
@@ -57,12 +57,44 @@ def collect_hospitals():
             r = requests.get(exacturl, allow_redirects=True)
             open(str(url[0]) + '.xlsx', 'wb').write(r.content)
         else:
-            print(url[0] + " not downloadable")
+            return url[0] + " not downloadable"
 
 def collect_insurers():
-    pass
+    def if_button(string):
+        keywords = 'TBD'
+        numin = 0
+        for stri in keywords:
+            if stri in string.lower():
+                numin +=1
+        if numin >= 2:
+            return True
+        else:
+            return False
+    for url in IPs:
+            
+        location = url[2]
+        firstvar = location.split('/')
+
+        domain = "https://" + firstvar[2]
+
+        result = requests.get(location)
+        soup = BeautifulSoup(result.text, "html.parser")
+
+        occurences = []
+
+        for link in soup.find_all('a'):
+            occurences.append(str(link.get('href')))
+
+        for string in occurences:
+            if if_button(string.lower()):
+                exacturl = domain + string
+
+        if is_downloadable(exacturl):
+            r = requests.get(exacturl, allow_redirects=True)
+            open(str(url[0]) + '.xlsx', 'wb').write(r.content)
+        else:
+            return url[0] + " not downloadable"
+
 
 def find_price(CPTCode, hospital):
     pass
-
-collect_hospitals()
